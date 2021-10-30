3e39e91527aebbb5869b7930683b5b5e395bfbfd
ExoRigger <tehvoid@protonmail.com>
2021-10-30 20:54:56 +0100

    modified:   export.py

--- export.py
+++ export.py
@@ -20,15 +20,38 @@
 
 # Imports
 import socket
+import os
+import shutil
 
-
-remote_server = ''
-remote_port = ''
-
-
+SERVER_REMOTE = ''
+PORT_REMOTE = 0
+INPUT_DIR = ''
+ARCHIVE_OUT= ''
+DIR_SIZE = os.path.getsize(directory)
 
 
 def export():
+    
+    # Compress directory before sending
+    shutil.make_archive(ARCHIVE_OUT,zip,INPUT_DIR)
+    
+    # Store compressed file location
+    # Create client socket
+    tx_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    tx_client.connect((SERVER_REMOTE,PORT_REMOTE))
+    tx_client.send(f"{ARCHIVE_OUT}")
+
+
+    # Open the archive in read mode and get the binary data, read 4kB at a time
+    # Send data and close the connection
+    with open(ARCHIVE_OUT,"rb") as f:
+        while True:
+            bytes_read = f.read(4096)
+	    if not bytes_read:
+	        break
+
+	    tx_client.sendall(bytes_read)
+    tx_client.close()
     return
 
 
