544439b2482fbe6927d94fbf63af0166e2c05c0d
ExoRigger <tehvoid@protonmail.com>
2021-10-30 21:19:15 +0100

    modified:   cc/server.py 	deleted:    cc/tcp_server_class.py

--- cc/server.py
+++ cc/server.py
@@ -1,22 +1,68 @@
-import socket
 
+
+import socket as sck
+import threading
+
+
+class Server_tcp:
+    
+    def __init__(self,bind_ip,bind_port):
+        self.bind_ip = bind_ip 
+        self.bind_port = bind_port 
+
+        self.server = sck.socket(sck.AF_INET,sck.SOCK_STREAM)
+        self.server.bind((bind_ip,bind_port))
+        
+        self.server.listen(5)
+        print("Listening on",bind_ip,bind_port)
+        
+        while True:
+            client,addr = self.server.accept()
+            print('Accepted connection from ',addr[0],addr[1])
+            client_handler = threading.Thread(target=self.handle_client,args=(client,))
+            client_handler.start()
+
+    def handle_client(self,client_socket):
+        RECV = client_socket.recv(4098).decode()
+        FILE = os.path.basename(RECV)
+        with open(FILE,"wb") as f:
+            while True:
+                bytes_read = client_socket.recv(4098)
+		if not bytes_read:
+		    break
+	  	f.write(bytes_read)
+
+	client_socket.close()
 
 
 
 
 
 def main():
+    # server = Server_tcp("127.0.0.1",6969)
+    return
 
-    SERVER_LOCAL = ''
-    PORT_LOCAL = 0
 
-    BUFFER_SIZE = 4096
-    SEPARATOR = ";"
 
-    server = socket.socket(SOCKET.AF_NET,SOCKET.SOCK_STREAM)
 
-    server.bind((SERVER_LOCAL,PORT_LOCAL))
-    server.listen(5)
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
